7fa47bb0f166382782fbbc891a865912949f2fcb
Putrilalala <Putri.sandianugraha@gmail.com>
2024-12-18 11:30:17 +0700

    v.0.3
    dark mode

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-// import Homepage from './Components/Homepage.jsx';
+import Homepage from './Components/Homepage.jsx';
 // import ProceduralTree from './ProceduralTree';
 // import Task from './Components/Task.jsx';
 import Dashboard from './Components/Dashboard.jsx';
@@ -12,8 +12,8 @@ function App() {
       {/* <LoginSignUp/> */}
       {/* <Homepage /> */}
       {/* <Task /> */}
-      {/* <Homepage /> */}
-      <Dashboard />
+      <Homepage />
+      {/* <Dashboard /> */}
       {/* <Board /> */}
 
     </div>
